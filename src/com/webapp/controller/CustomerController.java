2a8d6db43c0df4a8d70f00c90e94fade16564f2e
Amadou Ly <amly212782@gmail.com>
2018-04-02 20:26:27 +0200

    Customer controller up Adding a bootstrap template

--- src/com/webapp/controller/CustomerController.java
+++ src/com/webapp/controller/CustomerController.java
@@ -1,8 +1,13 @@
 package com.webapp.controller;
 
+import com.webapp.dao.CustomerDAO;
+import com.webapp.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by owner on 18-04-01.
@@ -11,8 +16,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customer")
 public class CustomerController {
 
+    @Autowired
+    private CustomerDAO customerDAO;
+
     @RequestMapping("/list")
     public String listCustomers(Model theModel) {
+
+        // get the list of students fro the DAO (Data Access Object) helper
+        List<Customer> customerList = customerDAO.getCustomers();
+
+        // set it as an attribute inside the Model
+        theModel.addAttribute("customers", customerList);
+
         return "list-customers";
     }
 }
